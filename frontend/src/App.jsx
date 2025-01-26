6372994c262a6e088a447aa193490c76d68bc035
Prasad Gurude <prasadgurude76@gmail.com>
2025-01-26 19:15:56 +0530

    Home page just giving a url or connecting to a http or ws is remaining

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,18 +3,23 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Home from './pages/Home'
 import EditorPage from './pages/EditorPage'
+import { Toaster } from 'react-hot-toast';
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />}             ></Route>
-        <Route path="/home" element={<Home />}         ></Route>
-        <Route path="/editor" element={<EditorPage />} ></Route>
-      </Routes>
-    </BrowserRouter>
+    <div>
+      <Toaster position='top-right' />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}                     ></Route>
+          <Route path="/home" element={<Home />}                 ></Route>
+          <Route path="/editor/:roomId" element={<EditorPage />} ></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
   )
 }
 
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,20 +1,53 @@
 import React, { useState } from 'react'
+import toast from 'react-hot-toast';
+import { useNavigate } from 'react-router-dom';
+import {v4} from 'uuid'
+
 
 function Home() {
+
+    const nvaigate = useNavigate()
 
     const [formData, setFormData] = useState({
         roomId: '',
         username: ''
     });
 
-    function handleChange(e) {
+    const handleChange = (e) => {
         setFormData({
             ...formData,
             [e.target.name]: e.target.value  // Dynamically update the correct field
         });
     }
 
+    const createNewRoom = () => {
+        const newRoomId = v4();  // Generates a unique room ID
+        setFormData({ ...formData, roomId: newRoomId }); // Set the generated ID
+        toast.success('new room created')
+    }
+
+    const joinRoom = () => {
+        if(!formData.roomId || !formData.username){
+            toast.error('ROOM ID & USERNAME both are required')
+            return;
+        }
+
+        nvaigate(`/editor/${formData.roomId}`,{
+            state:{
+                formData
+            }
+        })
+
+    }
+
+    const handleInputEnter = (e) => {
+        if(e.code ==="Enter"){
+            joinRoom();
+        }
+    }
+
     return (
+        
         <div className='homePageWrapper flex justify-center items-center h-screen bg-slate-950 text-white flex-col'>
             <div className='formWrapper flex flex-col w-96 justify-center space-y-3 p-4 bg-slate-900 rounded-sm shadow-green-950 shadow-2xl'>
                 <img src="/code-sync.png" alt="code-sync-logo" className='h-20 w-40' />
@@ -22,23 +55,27 @@ function Home() {
                 <input
                     type="text"
                     name="roomId"
-                    onChange={() => handleChange}
+                    onChange={handleChange}
                     value={formData.roomId}
                     placeholder='ROOM ID'
+                    onKeyUp={handleInputEnter}
                     className=' bg-white text-gray-700 p-1 pl-2 font-sans rounded-lg' />
                 <input
                     type="text"
                     name="username"
-                    onChange={() => handleChange}
+                    onChange={handleChange}
                     value={formData.username}
+                    onKeyUp={handleInputEnter}
                     placeholder='USERNAME'
                     className=' bg-white text-gray-600 p-1 pl-2 font-sans rounded-lg' />
-                <button className='bg-green-600  p-2 py-1 w-20 rounded-md self-end' >Join</button>
-                <p className='text-center' >If you don't have an invitethen create <span className='text-green-600 underline' >new room</span></p>
+                <button
+                onClick={joinRoom}
+                 className='bg-green-600  p-2 py-1 w-20 rounded-md self-end' >Join</button>
+                <p className='text-center' >If you don't have an invitethen create <span onClick={createNewRoom} className='text-green-600 underline cursor-pointer'>new room</span></p>
             </div>
             <footer className='fixed bottom-5'>
-                <h4>Build with 💛 by <a href="https://github.com/PrasadGurude" className='text-green-600'>Prasad Gurude</a> </h4>
-            </footer>
+                <h4>Build with 💛 by <a href="https://github.com/PrasadGurude" className='text-green-600 cursor-pointer'>Prasad Gurude</a> </h4>
+            </footer>            
         </div>
     )
 }
